b31908ba9963398baf924a87142d40f3e562d125
coollpice <coollpice@nvaer.com>
2022-11-30 18:50:30 +0900

    1. Mybatis 적용. 2. JPA 설정완료 -> 적용단계 시작

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,12 +2,18 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name") //생략가능 기본전략이 camel to under
     private String itemName;
+
     private Integer price;
     private Integer quantity;
 
